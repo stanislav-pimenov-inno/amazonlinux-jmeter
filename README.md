d80f2905817b384571b6cbde0baf2d4aabdfe03a
Stanislav Pimenov <stanislav_pimenov@epam.com>
2019-03-14 16:09:31 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # amazonlinux-jmeter
+
+## Install jmeter
+
+To install JDK8 and Jmeter 5.1 with plugin on the Amazon Linux instance execute: `sudo install-jmeter.sh`
+
+## Execute JMX test plan
+ 
---
+++ install-jmeter.sh
@@ -0,0 +1,19 @@
+yum install java-1.8.0-openjdk
+yum remove java-1.7.0-openjdk
+
+JMETER_VERSION="5.1"
+CMDRUNNER_VERSION="2.2"
+
+curl -fsSL --compressed -o /tmp/jmeter.tgz https://archive.apache.org/dist/jmeter/binaries/apache-jmeter-$JMETER_VERSION.tgz \
+    && tar -C /opt -xzf /tmp/jmeter.tgz \
+    && rm /tmp/jmeter.tgz \
+    && curl -fsSL --compressed -o /opt/apache-jmeter-$JMETER_VERSION/lib/ext/jmeter-plugins-manager.jar https://jmeter-plugins.org/get \
+    && curl -fsSL --compressed -o /opt/apache-jmeter-$JMETER_VERSION/lib/cmdrunner-$CMDRUNNER_VERSION.jar http://central.maven.org/maven2/kg/apc/cmdrunner/$CMDRUNNER_VERSION/cmdrunner-$CMDRUNNER_VERSION.jar \
+    && java -cp /opt/apache-jmeter-$JMETER_VERSION/lib/ext/jmeter-plugins-manager.jar org.jmeterplugins.repository.PluginManagerCMDInstaller \
+    && /opt/apache-jmeter-$JMETER_VERSION/bin/PluginsManagerCMD.sh install-all-except jpgc-hadoop,jpgc-oauth \
+    && sleep 2 \
+    && /opt/apache-jmeter-$JMETER_VERSION/bin/PluginsManagerCMD.sh status
+
+
+echo "export JMETER_HOME=/opt/apache-jmeter-$JMETER_VERSION" >> ~/.bashrc
+echo "export PATH=\$PATH:/opt/apache-jmeter-$JMETER_VERSION/bin" >> ~/.bashrc
